cfa757a9ab20e3794072d7ce7fdfb34668bcfe6d
Rebeca Coli <69267756+becacoli@users.noreply.github.com>
2021-12-19 03:15:19 -0300

    primeiraquestao

---
+++ SegundaLista/primeiraquestao.py
@@ -0,0 +1,9 @@
+#Faça um programa que leia dois números A e B e imprima o maior deles.
+
+A = int(input("Digite um valor para A:\n"))
+B = int(input("Digite um valor para B:\n"))
+
+if A > B:
+  print("O maior valor é", A)
+else:
+  print("O maior valor é", B)
