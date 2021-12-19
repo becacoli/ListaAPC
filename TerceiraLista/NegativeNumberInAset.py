c49df5342fb6c4cf0357f87b214b1c2ccbef5df7
Rebeca Coli <69267756+becacoli@users.noreply.github.com>
2021-12-19 09:44:20 -0300

    negativenumber

---
+++ TerceiraLista/NegativeNumberInAset.py
@@ -0,0 +1,7 @@
+valores = [-5,-3,-2, 3, 6, -8, -57, -1]
+numeros_negativos = 0
+
+for n in valores:
+  if n < 0:
+   numeros_negativos = numeros_negativos + 1        
+print("O número de valores negativos é", numeros_negativos)
